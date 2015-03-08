1124249d96a7ce93902e020a23fd9a48debea03a
Olena Domanska <odomanska@malkosua.com>
2015-03-08 10:40:57 +0200

    plot2

--- plot1.R
+++ plot1.R
@@ -1,13 +1,7 @@
 data=read.table("household_power_consumption.txt",header=TRUE,sep=";",na.string="?")
-> attach(data)
-The following objects are masked from data (pos = 3):
-
-    Date, Global_active_power, Global_intensity, Global_reactive_power,
-    Sub_metering_1, Sub_metering_2, Sub_metering_3, Time, Voltage
+attach(data)
 Date1=as.Date(Date,format="%d/%m/%Y",origin="16/12/2006")
 Date=Date1
-> data1=data[Date=="2007-02-01"|Date=="2007-02-02",]
-> dim(data1)
-[1] 2880    9
-> hist(data1$Global_active_power,xlab="Global Active Power (kilowatts)",col="red",main ="Global Active Power")
+data1=data[Date=="2007-02-01"|Date=="2007-02-02",]
+hist(data1$Global_active_power,xlab="Global Active Power (kilowatts)",col="red",main ="Global Active Power")
 
---
+++ plot2.R
@@ -0,0 +1,8 @@
+setwd("C:/Users/odomanska/Desktop/Coursera")
+data=read.table("household_power_consumption.txt",header=TRUE,sep=";",na.string="?")
+Date1=as.Date(data$Date,format="%d/%m/%Y",origin="16/12/2006")
+data$Date=Date1
+data1=data[data$Date=="2007-02-01"|data$Date=="2007-02-02",]
+x <- paste(data1$Date, data1$Time)
+data1$Time=strptime(x,"%Y-%m-%d %H:%M:%S")
+plot(data1$Time,data1$Global_active_power,type="l",xlab='',ylab="Global Active Power (kilowatts)")
